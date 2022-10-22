5ba3309554dbf5aa55c50da1f3f39ee92643f0d9
Anusha N S S R <nssranusha@yahoo.co.in>
2022-10-22 22:59:44 +0530

    Commit: Server.js

--- server.js
+++ server.js
@@ -1,68 +1,71 @@
 //Import the express and use it to listen to the port
-const express = require('express');
+const express = require("express");
 const app = express();
 //Using the port 8000
-const port = 8000;
+const port = process.env.PORT || 8000;
 //to import the express layouts
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
 //db connection
-const db = require('./config/mongoose');
+const db = require("./config/mongoose");
 //use the express session
-const session = require('express-session');
+const session = require("express-session");
 //to mange the .env files
-require('dotenv').config();
+require("dotenv").config();
 
 // for authentication
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
 
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 //to dsiplay the flash messages to user
-const flash = require('connect-flash');
-const customMware = require('./config/middleware');
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
 
 //to parse teh data
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('cookie-parser')());
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(require("cookie-parser")());
 
 //to use the files in the assets folder
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 //use the express layouts to load layout
 app.use(expressLayouts);
 
-//to let the app use the styling and scripst and apply to layout 
-app.set('layout extractStyles', true);
-app.set('layout extractLayouts', true);
+//to let the app use the styling and scripst and apply to layout
+app.set("layout extractStyles", true);
+app.set("layout extractLayouts", true);
 
 //set the app's view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //setting the path of the views
-app.set('views', './views');
+app.set("views", "./views");
 
-//session to authenticate 
-app.use(session({
+//session to authenticate
+app.use(
+  session({
     name: "Placement Cell",
     secret: process.env.LOCAL_SECRET,
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: (1000 * 60 * 100)
+      maxAge: 1000 * 60 * 100,
     },
-    store: MongoStore.create({
+    store: MongoStore.create(
+      {
         mongoUrl: process.env.DATABASE_URL,
-        autoRemove: 'disabled'
-    },
-        function (err) {
-            // console.log(err || 'connect-mongodb setup ok');
-        })
-}));
+        autoRemove: "disabled",
+      },
+      function (err) {
+        // console.log(err || 'connect-mongodb setup ok');
+      }
+    ),
+  })
+);
 
 //initialize the passport and get session
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 //set current user's usage
 app.use(passport.setAuthenticatedUser);
@@ -72,13 +75,13 @@ app.use(flash());
 app.use(customMware.setFlash);
 
 //use the routes path to get the rouites
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 //make the app listen to call on port and respond
 app.listen(port, function (err) {
-    if (err) {
-        //if up and running
-        console.log(`Error in loading server ${err}`);
-    }
-    console.log(`Server is up and running on port ::${port}`);
+  if (err) {
+    //if up and running
+    console.log(`Error in loading server ${err}`);
+  }
+  console.log(`Server is up and running on port ::${port}`);
 });
